cd497b35715bb7661bec928c117bb0354cbe7169
Devansh Gupta <devansh.codee@gmail.com>
2025-06-23 23:22:11 +0530

    new file

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("hello github from a beginner");
+console.log("hello github from a beginner");
+console.log("hello github from a beginner");
+console.log("hello github from a beginner");
